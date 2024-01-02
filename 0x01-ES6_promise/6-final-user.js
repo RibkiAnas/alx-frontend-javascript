817736e31e89c93042253053e26c94dcefca7aef
Anas Ribki <anasribki147@gmail.com>
2024-01-02 22:12:44 +0100

    Edit task: 6. Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    results.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    results.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
+  }
+  return results;
 }
